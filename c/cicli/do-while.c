e6a213a2d7f96fc2618d1b49a5e0744ed4436dfb
lotation <xlapsiu@gmail.com>
2019-07-26 22:05:29 +0200

    first commit

--- c/cicli/do-while.c
+++ c/cicli/do-while.c
@@ -3,11 +3,12 @@ int main () {
     int num;
     do
     {
-        printf("\nInsert a number\n");
+        printf("\nInsert a number between 0 and 20\n");
         scanf("%d",&num);
     } while (num!=13);
         printf("\nTerminated because number 13 appeared!\n");
         printf("(just like pokemons)\n");
 
     return 0;
-    
+
+}
