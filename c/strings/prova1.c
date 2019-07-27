88e2faac308ec8d35394b2fc8b3068962b21fa3d
lotation <xlapsiu@gmail.com>
2019-07-27 21:34:13 +0200

    exercises updated

---
+++ c/strings/prova1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char s1[50],s2[50];
+
+  printf("Inserisci due stringhe\n" );
+  scanf("%s %s\n", s1[50],s2[50]);
+
+  long int a = strlen(s1);
+  long int b = strlen(s2);
+
+  printf("La prima stringa contiene %ld caratteri   ", a);
+  printf("La seconda stringa contiene %ld caratteri\n", b);
+
+
+
+
+
+  return 0;
+}
